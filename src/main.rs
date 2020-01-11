afeeb34093d4462394db009a1110795f26f7d0b4
Bobur <bobs4462@gmail.com>
2020-01-12 00:57:39 +0300

    set up a basic router

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use futures::stream::StreamExt;
 // use futures::Future;
 // use http;
-use httparse::{Request, Status};
-#[macro_use]
+// #[macro_use]
 extern crate serde_json;
-use serde_json::Value;
+// use serde_json::Value;
 use std::process;
+use tachyon::request::HttpRequest;
 use tachyon::utils;
 use tera::{Context, Tera};
 use tokio::net::{TcpListener, TcpStream};
@@ -14,17 +14,16 @@ use tokio::prelude::*;
 #[macro_use]
 extern crate lazy_static;
 
-#[tokio::main]
+#[tokio::main(core_threads = 8)]
 async fn main() {
     let mut listener = TcpListener::bind(utils::address_get()).await.unwrap();
     let mut incoming = listener.incoming();
     let server = async move {
         while let Some(con) = incoming.next().await {
             match con {
-                Ok(socket) => {
-                    // println!("Got some connection from {:?}", socket.peer_addr());
-                    let doc = Render::new("link.html".to_string(), Context::new());
-                    tokio::spawn(doc.render(socket));
+                Ok(mut socket) => {
+                    let buf = HttpRequest::read(&mut socket).await;
+                    tokio::spawn(route(buf, socket));
                 }
                 Err(err) => {
                     eprintln!("Error on getting connection = {:?}", err);
@@ -38,69 +37,70 @@ lazy_static! {
     pub static ref TERA: Tera = match Tera::new("templates/**/*") {
         Ok(t) => t,
         Err(e) => {
-            eprintln!("Error obtaining address: {}", e);
+            eprintln!("Error reading template database {}", e);
             process::exit(1);
         }
     };
 }
 
-struct Render {
-    template: String,
-    context: Context,
+pub async fn route(buf: Vec<u8>, socket: TcpStream) {
+    let mut headers = [httparse::EMPTY_HEADER; 18];
+    let rqst = HttpRequest::new(&buf, &mut headers).unwrap();
+    let path = rqst.path.clone();
+    match path.unwrap().split('/').nth(1) {
+        Some("render") => render(rqst, socket).await,
+        Some("raw") => template_read(rqst, socket).await,
+        Some(_action) => {}
+        None => {}
+    }
 }
 
-impl Render {
-    pub fn new(template: String, context: Context) -> Self {
-        Render { template, context }
+pub async fn template_read<'a, 'b>(rqst: HttpRequest<'a, 'b>, mut socket: TcpStream) {
+    let content = match tokio::fs::read(
+        "templates/".to_string() + rqst.path.unwrap().split('/').last().unwrap(),
+    )
+    .await
+    {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error reading file {}", e);
+            process::exit(1);
+        }
+    };
+    if let Err(e) = socket
+        .write_all(
+            &[
+                "HTTP/1.1 200 OK\r\ncontent-type: text/html; charset=UTF-8\r\n\r\n".as_bytes(),
+                &content[..],
+            ]
+            .concat(),
+        )
+        .await
+    {
+        panic!("Error writing response: {}", e);
     }
-    pub async fn render(self, mut socket: TcpStream) {
-        let mut buf = [0_u8; 512];
-        let mut heap_buf: Vec<u8> = Vec::new();
-        let mut char_num = 0;
-        loop {
-            let l = socket.read(&mut buf[..]).await.unwrap();
-            char_num += l;
-            heap_buf.append(&mut buf.to_vec());
-            if l != 512 {
-                break;
-            }
-        }
+    println!("READ DONE! {}", String::from_utf8(content).unwrap());
+    println!("WRITE DONE!");
+    let _ = socket.shutdown(std::net::Shutdown::Write);
+}
 
-        let mut headers = [httparse::EMPTY_HEADER; 18];
-        let mut req = Request::new(&mut headers);
-        let res = req.parse(&heap_buf[..]).unwrap();
-        // println!(
-        //     "req: {:?}\n{}",
-        //     req,
-        //     String::from_utf8(heap_buf.clone()).unwrap()
-        // );
-        let mut b: Value = json!(null);
-        if let Status::Complete(stop) = res {
-            // println!("char len {}", stop);
-            // println!("last char: {}", heap_buf[stop]);
-            heap_buf.truncate(char_num);
-            if heap_buf[stop] != 0 {
-                b = serde_json::from_slice(&heap_buf[stop..]).unwrap();
-                // println!("{:?}", b);
-            }
-        }
-
-        // delay_for(Duration::from_secs(5)).await;
-
-        let body = match TERA.render(&self.template, &Context::from_value(b).unwrap()) {
-            Ok(body) => body,
-            Err(e) => panic!("Error rendering html: {}", e),
-        };
-        if let Err(e) = socket
-            .write_all(
-                ("HTTP/1.1 200 OK\r\ncontent-type: text/html; charset=UTF-8\r\n".to_string()
-                    + &body)
-                    .as_bytes(),
-            )
-            .await
-        {
-            panic!("Error writing response: {}", e);
-        }
-        let _ = socket.shutdown(std::net::Shutdown::Write);
+pub async fn render<'a, 'b>(rqst: HttpRequest<'a, 'b>, mut socket: TcpStream) {
+    let body = match TERA.render(
+        rqst.path.unwrap().split('/').last().unwrap(),
+        &Context::from_value(serde_json::from_str(rqst.body.unwrap()).unwrap()).unwrap(),
+    ) {
+        Ok(body) => body,
+        Err(e) => panic!("Error rendering html: {}", e),
+    };
+    if let Err(e) = socket
+        .write_all(
+            ("HTTP/1.1 200 OK\r\ncontent-type: text/html; charset=UTF-8\r\n\r\n".to_string()
+                + &body)
+                .as_bytes(),
+        )
+        .await
+    {
+        panic!("Error writing response: {}", e);
     }
+    let _ = socket.shutdown(std::net::Shutdown::Write);
 }
--- src/request.rs
+++ src/request.rs
@@ -1,1 +1,66 @@
+use httparse::{Header, Request, Status};
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+const BUF_SIZE: usize = 4096;
 
+#[derive(Debug)]
+pub struct HttpRequest<'headers, 'buf: 'headers> {
+    /// The request method, such as `GET`.
+    pub method: Option<&'buf str>,
+    /// The request path, such as `/about-us`.
+    pub path: Option<&'buf str>,
+    /// The request headers.
+    pub headers: &'headers mut [Header<'buf>],
+    /// The request body.
+    pub body: Option<&'buf str>,
+}
+
+impl<'a, 'b: 'a> HttpRequest<'a, 'b> {
+    pub fn new(
+        buf: &'b Vec<u8>,
+        headers: &'a mut [Header<'b>],
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut req = Request::new(headers);
+        let res = req.parse(&buf[..])?;
+        let mut body = None;
+        if let Status::Complete(stop) = res {
+            if buf.len() > stop {
+                body = Some(std::str::from_utf8(&buf[stop..buf.len()])?);
+            }
+        }
+        let Request {
+            method,
+            path,
+            version: _,
+            headers,
+        } = req;
+        Ok(HttpRequest {
+            method: method,
+            path: path,
+            headers: headers,
+            body: body,
+        })
+    }
+
+    pub async fn read(socket: &mut TcpStream) -> Vec<u8> {
+        let mut buf = [0_u8; BUF_SIZE];
+        let mut heap_buf: Vec<u8> = Vec::new();
+        let mut l;
+        loop {
+            l = socket.read(&mut buf[..]).await.unwrap();
+            if l != BUF_SIZE && heap_buf.len() == 0 {
+                break;
+            } else if l != BUF_SIZE && heap_buf.len() != 0 {
+                heap_buf.append(&mut buf[..l].to_vec());
+                break;
+            } else {
+                heap_buf.append(&mut buf.to_vec());
+            }
+        }
+        if heap_buf.len() > 0 {
+            heap_buf
+        } else {
+            buf[..l].to_vec()
+        }
+    }
+}
