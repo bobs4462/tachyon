79aa3102a5a84c0d402c03c7402cca1a5beebab5
Bobur <bobs4462@gmail.com>
2020-02-22 02:53:24 +0300

    some refactors

--- gui/controllers/apidoc.js
+++ gui/controllers/apidoc.js
@@ -1,11 +1,47 @@
-var ApiDoc = Vue.component("api-doc", {
-	template: `
-<el-container style="height: 100%">
-<el-main>
-<div>Hello world</div>
-</el-main>
+const template = `
+<el-container>
+	<el-main>
+		<el-card>
+			<h2>Шаблоны</h2>
+			<el-card>
+				<h3>Введение</h3>
+
+				<div>
+					Любой шаблон является просто текстовым файлом, в котором переменные и выражения будут 
+					заменены значениями, при отрисовке (рендере). Синтаксис основан на шаблонах Jinja2 и Django.
+				</div>
+				<div>
+					Существует 3 вида разделителей, которые нельзя изменить:
+				</div>
+				<div>
+					<ul>
+						<li><code>{{</code> и <code>}}</code> для выражений</li>
+						<li><code>{%</code> или <code>{%-</code> и <code>%}</code> или <code>-%}</code> для операторов</li>
+						<li><code>{#</code> и <code>#}</code> для комментариев</li>
+					</ul>
+				</div>
+			</el-card>
+			<el-card>
+				<h3>Без отрисовки</h3>
+				<div>
+					Весь текст внутри блока raw как строку и не будет пытаться отобразить
+					то, что внутри. Полезно, если у вас есть текст, содержащий разделители.
+				</div>
+				<div>
+					<p><code>{%</code></span><span style="color:#f92672;">raw </span><span style="color:#75715e;"><code>%}</code></p>
+					<p><code>Здравствуйте {{</code><code>name}}</code></p>
+					<p><code>{% </span><span style="color:#f92672;">endraw</span><span style="color:#75715e;"><code>%}</code></p>
+				</div>
+
+				<p>будет отображаться как <code>Здравствуйте {{</code><code>name}}</code>.</p>
+			</el-card>
+		</el-card>
+	</el-main>
 </el-container>
-	`,
+
+`
+var ApiDoc = Vue.component("api-doc", {
+	template: template,
 	data: function() {
 		return {
 			h: false
