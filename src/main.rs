9b1fcdef4e1eb80e84903ba0f66af4ea6d5044f7
Bobur <bobs4462@gmail.com>
2020-02-25 01:51:45 +0300

    CI configurations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use futures::stream::StreamExt;
+use std::io::Write;
 use tachyon::router::route;
 use tokio::net::TcpListener;
 
 #[tokio::main(core_threads = 4)]
 async fn main() {
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("logs/pid")
+        .unwrap();
+    f.write(format!("{}", std::process::id()).as_bytes())
+        .expect("FAILED TO WRITE PID");
+    drop(f);
+
     let mut listener = TcpListener::bind(tachyon::utils::address_get())
         .await
         .unwrap();
